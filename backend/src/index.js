468b29866ef369fc82937d2a7287fac3c15b9081
Marcelo Ribeiro Silva <ribmarc@gmail.com>
2020-04-07 23:44:22 -0300

    Montando back

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,35 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (request, response)=>{
-  return response.json({ message: "Hello World" })
+app.get('/projects', (request, response)=>{
+  return response.json([
+    'Projeto1',
+    'Projeto2'
+  ])
 });
 
-app.listen(3333);
+app.post('/projects', (request, response) => {
+  return response.json([
+    'Projeto1',
+    'Projeto2'
+  ])
+})
+
+app.put('/projects/:id', (request, response) => {
+  return response.json([
+    'Projeto1',
+    'Projeto2'
+  ])
+})
+
+app.delete('/projects/:id', (request, response) => {
+  return response.json([
+    'Projeto2'
+  ])
+})
+
+app.listen(3333, ()=>{
+  console.log('🤑️ Back-End started!')
+});
