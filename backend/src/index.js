d61a8ae09b0a38cf280067ad3149d4dc5e295370
Marcelo Ribeiro Silva <ribmarc@gmail.com>
2020-04-10 22:49:56 -0300

    end node concepts

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,34 @@
 const express = require('express');
-const { uuid } = require('uuidv4');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 app.use(express.json());
 
 const projects = [];
+
+function logRequest(request, response, next){
+  const { method, url } = request;
+
+  const logLabel = `[${method.toUpperCase()}] - ${url}`;
+
+  console.log(logLabel);
+
+  return next();
+}
+
+function validateProjectId(request, response, next) {
+  const { id } = request.params;
+
+  if(!isUuid(id)){
+    return response.status(400).json({error: 'Invalid Project ID'});
+  }
+
+  return next();
+}
+
+app.use(logRequest);
+app.use('/projects/:id', validateProjectId);
+
 
 app.get('/projects', (request, response)=>{
   const { title } = request.query;
