7d19d26a6a4267e6986c29531ce8a16027ea5b47
Marcelo Ribeiro Silva <ribmarc@gmail.com>
2020-04-13 20:50:42 -0300

    front e back finalizados

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './App.css';
-import backgroundImage from './assets/background.jpeg';
 
 import Header from './components/Header';
 
 function App() {
   
-  const [projects, setProjects] = useState(['Desenvolvimento mobile','Desenvolvimento web']);
+  const [projects, setProjects] = useState([]);
 
-  function handleAddProject(){
-    setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    });
+  }, []);
+
+  async function handleAddProject(){
+    // setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+
+    const response = await api.post('projects', {
+      title:`Novo Projeto ${Date.now()}`,
+      owner:"Marcelo Silva"
+    });
+
+    const project = response.data;
+    setProjects([...projects, project]);
   }
 
   return (
     <>      
       <Header title="Projects"/>
 
-      <img width={350} src={backgroundImage} />
-
       <ul>
-        {projects.map(project => <li key={project}>{project}</li>)}
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
       </ul>
 
       <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
