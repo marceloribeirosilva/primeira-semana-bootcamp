4021962237c7ef3d3686da761df77779e743e041
Marcelo Ribeiro Silva <ribmarc@gmail.com>
2020-04-15 17:56:08 -0300

    cors

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,13 +1,41 @@
-import React from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, StatusBar, Button } from 'react-native';
+
+import api from './services/api'
 
 export default function App(){
+  const [projects, setProjects] = useState([]);
+
+  function handleGetProjects() {
+    api.get('/projects').then(response => {
+      console.log(response.data)
+      //setProjects(response.data);
+    })
+  };
+
+  // useEffect(() => {
+  //   try {
+  //     api.get('/projects').then(response => {
+  //       console.log(response.data)
+  //       //setProjects(response.data);
+  //     })
+
+  //   } catch (err) {
+
+  //     console.error(err);
+  //   }
+  // }, []);
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
       <View style={styles.container}>
         <Text style={styles.title}>Hello GoStack</Text>
       </View>
+      <Button 
+        onPress = {handleGetProjects}
+        title = "Get Projects"
+      />
     </>
   )
 }
