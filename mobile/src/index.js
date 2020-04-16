bde21b74bca8c3e2507c60865bb7fe5fe53d697c
Marcelo Ribeiro Silva <ribmarc@gmail.com>
2020-04-16 01:11:42 -0300

    Listagem de projetos

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,41 +1,44 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, StatusBar, Button } from 'react-native';
+import { SafeAreaView, FlatList ,Text, StyleSheet, StatusBar, Button } from 'react-native';
 
 import api from './services/api'
 
 export default function App(){
   const [projects, setProjects] = useState([]);
 
-  function handleGetProjects() {
-    api.get('/projects').then(response => {
-      console.log(response.data)
-      //setProjects(response.data);
-    })
-  };
 
-  // useEffect(() => {
-  //   try {
-  //     api.get('/projects').then(response => {
-  //       console.log(response.data)
-  //       //setProjects(response.data);
-  //     })
+  useEffect(() => {
+    try {
+      api.get('/projects').then(response => {        
+        setProjects(response.data);
+      })
 
-  //   } catch (err) {
+    } catch (err) {
 
-  //     console.error(err);
-  //   }
-  // }, []);
+      console.error(err);
+    }
+  }, []);
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello GoStack</Text>
-      </View>
-      <Button 
-        onPress = {handleGetProjects}
-        title = "Get Projects"
-      />
+
+      <SafeAreaView style={styles.container}>
+        <FlatList          
+          data={projects}
+          keyExtractor={project => project.id}
+          renderItem={({ item }) => (
+            <Text style={styles.project}>{item.title}</Text>
+          )}
+        />
+      </SafeAreaView>
+      
+      
+      {/* <View style={styles.container}>
+        {projects.map(project => (
+          <Text style={styles.project} key={project.id}>{project.title}</Text>
+        ))}
+      </View> */}
     </>
   )
 }
@@ -44,13 +47,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#7159c1',
-    justifyContent: 'center',
-    alignItems: 'center'
   },
 
-  title: {
+  project: {
     color: '#fff',
-    fontSize: 30,
-    fontWeight: 'bold'
+    fontSize: 30,    
   }
 });
