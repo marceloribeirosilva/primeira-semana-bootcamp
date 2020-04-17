32899151687f469c2b5af666eb974eeb5ffd7a86
Marcelo Ribeiro Silva <ribmarc@gmail.com>
2020-04-17 17:10:57 -0300

    Término do mobile

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, FlatList ,Text, StyleSheet, StatusBar, Button } from 'react-native';
+import { SafeAreaView, FlatList ,Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
 
 import api from './services/api'
 
@@ -19,6 +19,17 @@ export default function App(){
     }
   }, []);
 
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      title: `Novo Projeto ${Date.now()}`,
+      owner: 'Marcelo Silva'
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+  }
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
@@ -31,6 +42,13 @@ export default function App(){
             <Text style={styles.project}>{item.title}</Text>
           )}
         />
+        <TouchableOpacity 
+          activeOpacity={0.6} 
+          style={styles.button} 
+          onPress={handleAddProject}
+        >
+          <Text style={styles.buttonText}>Adicionar Projeto</Text>
+        </TouchableOpacity>
       </SafeAreaView>
       
       
@@ -52,5 +70,19 @@ const styles = StyleSheet.create({
   project: {
     color: '#fff',
     fontSize: 30,    
+  },
+
+  button: {
+    backgroundColor: '#fff',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 16,
   }
 });
